6687ecadf0eee125744ca8be32c6751dc5532768
Helloworld20-web <cadion.estrella68@gmail.com>
2026-01-07 13:04:34 +0400

    Add files via upload

---
+++ seaboned.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+seaboneds = pd.read_csv("Salary.csv")
+
+#Main Plots
+sns.set_style("darkgrid")
+sns.color_palette("viridis")
+
+sns.scatterplot(x="YearsExperience", y="Salary", data=seaboneds)
+
+sns.pairplot(seaboneds)
+
+sns.relplot(x="YearsExperience", y="Salary", data=seaboneds, kind="scatter")
+
+sns.lineplot(x="YearsExperience", y="Salary", data=seaboneds)
+
+sns.displot(seaboneds, kind="ecdf")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="strip")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="violin")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="bar")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="point")
+
+sns.lmplot(x="YearsExperience", y="Salary", data=seaboneds, hue="YearsExperience")
+
+plt.show()
+
+#Heatmaps
+sns.heatmap(seaboneds.corr(), annot=True)
+sns.clustermap(seaboneds)
+
+plt.show()
+
+#Useless Stuff
+sns.histplot(seaboneds, bins=10, kde=True)
+
+sns.displot(seaboneds, kind="kde")
+
+sns.displot(seaboneds, kind="hist")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="box")
+
+sns.catplot(x="YearsExperience", y="Salary", data=seaboneds, kind="swarm")
+
+sns.catplot(data=seaboneds, kind="count")
+
+sns.regplot(x="YearsExperience", y="Salary", data=seaboneds)
+
+plt.show()
